5bf213d3b736689b78988b86cc4d1187a58a37eb
dilipkn007 <dilipkn13082001@gmail.com>
2021-11-02 22:33:16 +0530

    first commit

---
+++ js.js
@@ -0,0 +1,13 @@
+var msg=new SpeechSynthesisUtterance()
+const speech = (alphabet) => {
+    msg.text=alphabet
+    window.speechSynthesis.speak(msg)
+    console.log(alphabet)
+}
+const x = document.getElementsByClassName("alph")
+for (let index = 0; index < x.length; index++) {
+    x[index].addEventListener("mouseover", function () {
+        speech(x[index].innerHTML[0])
+    })
+}
+
